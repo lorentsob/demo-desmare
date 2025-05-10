7d5fe59d4e49abfa4569b9756f6b36bb534eedec
lorentsob <lorenzoboschi39@gmail.com>
2025-05-10 14:57:56 +0200

    Enhance Next.js configuration by adding custom Webpack cache settings and importing the 'path' module.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
     // This makes Next.js optimize the images in public directory too
     unoptimized: false,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Limita la dimensione della cache webpack
+    config.cache = {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename]
+      },
+      cacheDirectory: path.resolve(__dirname, '.next/cache'),
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 1 settimana
+    };
+    return config;
   },
 }
 
